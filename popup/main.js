df96a14ab1e26a688abd937280f6dc220be5531b
dietrich ayala <autonome@gmail.com>
2020-03-07 13:34:24 -0500

    import all the popup and other stuff

---
+++ popup/main.js
@@ -0,0 +1,187 @@
+(async () => {
+
+const DEBUG = 0;
+
+dbg('POPUP INIT');
+
+let commands = {};
+
+function onCommandsUpdated () {
+  window.dispatchEvent(new CustomEvent('cmd-update-commands', { detail: commands }));
+  dbg('main sending updated commands out', Object.keys(commands))
+}
+
+window.addEventListener('DOMContentLoaded', initializeCommandSources);
+
+/*
+command is an object with two properties:
+
+- name: string label
+- execute: method
+
+TODO:
+- add canRun check - eg, switchContainer cannot run on about: urls
+- enable command generation at call time (instead of in advance)
+
+*/
+function addCommand(command) {
+  commands[command.name] = command;
+  onCommandsUpdated();
+}
+
+function initializeCommandSources() {
+  dbg('initializeCommandSources');
+  sourceBookmarklets();
+  sourceBookmark();
+  sourceEmail();
+  sourceGoogleDocs();
+  sourceSendToWindow();
+  sourceSwitchToWindow();
+  sourceNewContainerTab();
+  sourceSwitchTabContainer();
+  onCommandsUpdated();
+}
+
+async function sourceBookmarklets() {
+  // add bookmarklets as commands
+  let bmarklets = await browser.bookmarks.search({ query: 'javascript:'} );
+  bmarklets.map(b => {
+    return {
+      name: b.title,
+      async execute(cmd) {
+        //let tags = cmd.typed.split(' ').filter(w => w != cmd.name)
+        //console.log('tags', tags)
+        let tabs = await browser.tabs.query({active:true});
+        browser.tabs.executeScript(tabs[0].id, {
+          code: b.url.replace('javascript:', '')
+        });
+      }
+    };
+  }).forEach(addCommand);
+}
+
+async function sourceBookmark() {
+  addCommand({
+    name: 'bookmark current page',
+    async execute() {
+      let tab = await browser.tabs.query({active:true});
+      let node = await browser.bookmarks.create({
+        title: tab[0].title,
+        url: tab[0].url
+      });
+    }
+  });
+}
+
+async function sourceEmail() {
+  addCommand({
+    name: 'Email page to',
+    async execute(msg) {
+      let tabs = await browser.tabs.query({active:true});
+      let email = msg.typed.replace(msg.name, '').trim();
+      let url =
+        'mailto:' + email +
+        '?subject=Web%20page!&body=' +
+        encodeURIComponent(tabs[0].title) +
+        '%0D%0A' +
+        encodeURIComponent(tabs[0].url);
+      tabs[0].url = url;
+    }
+  });
+}
+
+async function sourceGoogleDocs() {
+  [
+    {
+      cmd: 'New Google doc',
+      url: 'http://docs.google.com/document/create?hl=en'
+    },
+    {
+      cmd: 'New Google sheet',
+      url: 'http://spreadsheets.google.com/ccc?new&hl=en'
+    }
+  ].forEach(function(doc) {
+    addCommand({
+      name: doc.cmd,
+      async execute(msg) {
+        await browser.tabs.create({
+          url: doc.url
+        });
+      }
+    });
+  });
+}
+
+async function sourceSendToWindow() {
+  const cmdPrefix = 'Move to window: ';
+  const windows = await browser.windows.getAll({windowTypes: ['normal']});
+  windows.forEach((w) => {
+    addCommand({
+      name: cmdPrefix + w.title,
+      async execute(msg) {
+        const activeTabs = await browser.tabs.query({active: true});
+        browser.tabs.move(activeTabs[0].id, {windowId: w.id, index: -1});
+      }
+    });
+  });
+}
+
+async function sourceSwitchToWindow() {
+  const cmdPrefix = 'Switch to window: ';
+  const windows = await browser.windows.getAll({});
+  windows.forEach((w) => {
+    addCommand({
+      name: cmdPrefix + w.title,
+      async execute(msg) {
+        browser.windows.update(w.id, { focused: true });
+      }
+    });
+  });
+}
+
+async function sourceNewContainerTab() {
+  const cmdPrefix = 'New container tab: ';
+  browser.contextualIdentities.query({})
+  .then((identities) => {
+    if (!identities.length)
+      return;
+    for (let identity of identities) {
+      addCommand({
+        name: cmdPrefix + identity.name,
+        async execute(msg) {
+          browser.tabs.create({url: '', cookieStoreId: identity.cookieStoreId });
+        }
+      });
+    }
+  });
+}
+
+async function sourceSwitchTabContainer() {
+  const cmdPrefix = 'Switch container to: ';
+  browser.contextualIdentities.query({})
+  .then((identities) => {
+    if (!identities.length)
+      return;
+    for (let identity of identities) {
+      addCommand({
+        name: cmdPrefix + identity.name,
+        async execute(msg) {
+          const activeTabs = await browser.tabs.query({currentWindow: true, active: true});
+          const tab = activeTabs[0];
+          browser.tabs.create({url: tab.url, cookieStoreId: identity.cookieStoreId, index: tab.index+1, pinned: tab.pinned }).then(() => {
+            // only remove old tab if new tab was created successfully
+            browser.tabs.remove(tab.id);
+          });
+        }
+      });
+    }
+  });
+}
+
+function dbg(...args) {
+  if (DEBUG == 1) {
+    console.log(...args)
+  }
+}
+
+})();
---
+++ popup/ui.js
@@ -0,0 +1,371 @@
+/*
+ 
+TODO: NOW
+* store state data in add-on, not localStorage
+
+TODO: NEXT
+* command suggestions (listed below - eg, see windows)
+* command parameters
+* switch to window command, searching by title (named windows?)
+* modular commands
+
+TODO: FUTURE
+* remember last-executed command across restarts
+* sessions (?)
+* add to Pocket command
+* configurable shortcut
+* better visual fix for overflow text
+* commands that identify things in the page and act on them (locations, events, people)
+
+*/
+
+(async () => {
+
+let state = {
+  commands: [], // array of command names
+  matches: [], // array of commands matching the typed text
+  matchIndex: 0, // index of ???
+  matchCounts: {}, // match counts - selectedcommand:numberofselections
+  matchFeedback: {}, // adaptive matching - partiallytypedandselected:fullname
+  typed: '', // text typed by user so far, if any
+  lastExecuted: '' // text last typed by user when last they hit return
+};
+
+window.addEventListener('cmd-update-commands', function(e) {
+  //console.log('ui received updated commands');
+  state.commands = e.detail;
+});
+
+async function render() {
+  // Outer container
+  let panel = document.createElement('div');
+  panel.id = 'cmdPanel';
+  panel.classList.add('cmdPanel');
+
+  await css(panel, {
+    //border: '1px solid black',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: '0',
+    padding: '0',
+    height: '3rem',
+    width: '20rem',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+  });
+
+  // Where text is shown
+  let input = document.createElement('div');
+  input.id = 'cmdInput';
+
+  await css(input, {
+    //border: '1px solid black',
+    //overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+    fontSize: 'large'
+  });
+
+  panel.appendChild(input);
+
+  document.body.appendChild(panel);
+
+  // add event listeners
+  document.addEventListener('keyup', onKeyup, true);
+  document.addEventListener('keypress', onKeyDummyStop, true);
+  document.addEventListener('keydown', onKeyDummyStop, true);
+  document.addEventListener('input', onKeyDummyStop, true);
+}
+
+render();
+
+async function css(el, props) {
+  Object.keys(props).forEach(p => el.style[p] = props[p]);
+}
+
+function execute(name, typed) {
+  if (state.commands[name]) {
+    //console.log('popup:execute()', name, typed);
+    state.commands[name].execute({typed});
+  }
+}
+
+function findMatchingCommands(text) {
+  const r = true;
+  r || console.log('findMatchingCommands', text, state.commands.length);
+  var count = state.commands.length,
+      matches = [];
+
+  // Iterate over all commands, searching for matches
+  //for (var i = 0; i < count; i++) {
+  //for (const [name, properties] of Object.entries(state.commands)) {
+  for (const name of Object.keys(state.commands)) {
+    // Match when:
+    // 1. typed string is anywhere in a command name
+    // 2. command name is at beginning of typed string
+    //    (eg: for command input - "weather san diego")
+    r || console.log('testing option...', name);
+    if (name.toLowerCase().indexOf(state.typed.toLowerCase()) != -1 ||
+        state.typed.toLowerCase().indexOf(name.toLowerCase()) === 0) {
+      matches.push(name);
+    }
+  }
+
+  // sort by match count
+  state.matches.sort(function(a, b) {
+    var aCount = state.matchCounts[a] || 0;
+    var bCount = state.matchCounts[b] || 0;
+    return bCount - aCount;
+  })
+
+  // insert adaptive feedback
+  if (state.matchFeedback[state.typed]) {
+    state.matches.unshift(state.matchFeedback[state.typed])
+  }
+
+  return matches;
+}
+
+function updateMatchFeedback(typed, name) {
+  state.matchFeedback[typed] = name;
+}
+
+function updateMatchCount(name) {
+  if (!state.matchCounts[name]);
+    state.matchCounts[name] = 0;
+  state.matchCounts[name]++;
+}
+
+async function shutdown() {
+  window.close();
+  /*
+  let container = document.querySelector('#cmdContainer');
+  if (container) {
+    document.body.removeChild(container);
+  }
+  document.removeEventListener('keyup', onKeyup, true);
+  document.removeEventListener('keypress', onKeyDummyStop, true);
+  document.removeEventListener('keydown', onKeyDummyStop, true);
+  document.removeEventListener('input', onKeyDummyStop, true);
+  */
+  console.log('ui shutdown complete');
+}
+
+function onKeyDummyStop(e) {
+  e.preventDefault();
+}
+
+async function onKeyup(e) {
+  // flag for logging
+  const r = true;
+
+  e.preventDefault();
+
+  if (isModifier(e)) {
+    return;
+  }
+
+  r || console.log('onKeyup', e.key, e.which)
+  r || console.log('hasModifier', hasModifier(e), 'isModifier', isModifier(e), 'isIgnorable', isIgnorable(e));
+
+  e.preventDefault();
+
+  // if user pressed escape, go away
+  if (e.key == 'Escape' && !hasModifier(e)) {
+    r || console.log('onKeyUp: escape!');
+    await shutdown();
+  }
+
+  // if user pressed return, attempt to execute command
+  else if (e.key == 'Enter' && !hasModifier(e)) {
+    r || console.log('onKeyUp: enter!', state.typed);
+    let name = state.matches[state.matchIndex];
+    if (Object.keys(state.commands).indexOf(name) > -1) {
+      //await shutdown();
+      execute(name, state.typed);
+      state.lastExecuted = name;
+      updateMatchCount(name);
+      updateMatchFeedback(state.typed, name);
+      state.typed = '';
+    }
+  }
+
+  // attempt to complete typed characters to a command
+  // or do other modifications based on user typed keys
+  else if (!hasModifier(e) && !isModifier(e) && !isIgnorable(e)) {
+    r || console.log('LEGIT... no modifier, is not a modifier and not ignorable')
+
+    // correct on backspace
+    if (e.key == 'Backspace') {
+      r || console.log('back', state.typed);
+      if (state.typed.length > 0) {
+        r || console.log('back, no typed tho');
+        state.typed = state.typed.substring(0, state.typed.length - 1);
+      }
+    }
+    // otherwise add typed character to buffer
+    else {
+      r || console.log('updating', e.key);
+      state.typed += e.key
+    }
+
+    // search, and update UI
+    state.matches = findMatchingCommands(state.typed);
+    if (state.matches.length) {
+      r || console.log('matches!', state.matches);
+      updateInputUI(state.typed, state.matches[0]);
+      state.matchIndex = 0;
+    }
+    else {
+      r || console.log('no matches for ', state.typed);
+      updateInputUI(state.typed);
+    }
+  }
+
+  // if up arrow and currently visible command is not first, select one previous
+  else if (e.key == 'ArrowUp' && state.matchIndex) {
+    r || console.log('onKeyUp: arrow up!');
+    updateInputUI(state.typed, state.matches[--state.matchIndex]);
+  }
+
+  // if down arrow and there are more matches, select the next one
+  else if (e.key == 'ArrowDown' && state.matchIndex + 1 < state.matches.length) {
+    r || console.log('onKeyUp: arrow down!');
+    updateInputUI(state.typed, state.matches[++state.matchIndex]);
+  }
+
+  // Old behavior on tab:
+  // tab -> shift to next result
+  // shift + tab -> shift to previous result
+  // New behavior on tab:
+  // autocomplete to the matched command
+  // which allows easy adding onto a command name
+  // without having to type all the same visible text
+  else if (e.key == 'Tab' && state.matches) {
+    r || console.log('onKeyUp: tab!');
+    state.typed = state.matches[state.matchIndex]
+    updateInputUI(state.typed, state.matches[state.matchIndex]);
+    //if (e.shiftKey && matchIndex)
+    //  updateInputUI(state.typed, state.matches[--state.matchIndex]);
+    //else if (state.matchIndex + 1 < state.matches.length)
+    //  updateInputUI(state.typed, state.matches[++state.matchIndex]);
+  }
+}
+
+function hasModifier(e) {
+  return e.altKey || e.ctrlKey || e.metaKey;
+}
+
+function isModifier(e) {
+  return ['Alt', 'Control', 'Shift', 'Meta'].indexOf(e.key) != -1;
+}
+
+function isIgnorable(e) {
+  switch(e.which) {
+    case 38: //up arrow
+    case 40: //down arrow
+    case 37: //left arrow
+    case 39: //right arrow
+    case 33: //page up
+    case 34: //page down
+    case 36: //home
+    case 35: //end
+    case 13: //enter
+    case 9:  //tab
+    case 27: //esc
+    case 16: //shift  
+    case 17: //ctrl  
+    case 18: //alt  
+    case 20: //caps lock 
+    // we handle this for editing
+    //case 8:  //backspace  
+    // need to handle for editing also?
+    case 46: //delete 
+    case 224: //meta 
+    case 0:
+      return true;
+      break;
+    default:
+      return false;
+  }
+}
+
+function updateInputUI(typed, completed) {
+  const r = true;
+  r || console.log('updateInputUI', typed, completed);
+  let str = ''
+  if (completed) {
+    str = generateUnderlined(typed, completed);
+  }
+  // no match
+  else if (typed) {
+    str = typed;
+  }
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(str, 'text/html');
+
+  let input = document.querySelector('#cmdInput');
+  if (input && input.firstElementChild)
+    input.removeChild(input.firstElementChild);
+  input.appendChild(doc.firstElementChild);
+}
+
+// typed text, inline matching suggestion
+function generateUnderlined(typed, match) {
+  const r = 1;
+  r || console.log('generateUnderlined', typed, match);
+  // user already matched a commmand and added params
+  /*
+  if (match.length > typed.length &&
+      match.indexOf(typed) === 0) {
+    return match;
+  }
+  */
+
+  if (typed.length == 0 || match.length == 0)
+    return typed;
+
+  // look for typed within match
+  var startIndex = match.toLowerCase().indexOf(typed.toLowerCase());
+  if (startIndex == -1) {
+    // otherwise look for match in typed
+    // (why would this happen?!)
+    startIndex = match.toLowerCase().indexOf(typed.toLowerCase());
+    if (startIndex == -1) {
+      return typed;
+    }
+  }
+
+  var endIndex = startIndex + match.length;
+  r || console.log('startIndex', startIndex, 'endIndex', endIndex);
+  var str = ''
+
+  // substring is empty
+  if (!match) {
+    r || console.log('no suggestion, so no underline')
+    str = '<span>' + typed + '</span>'
+  }
+  // occurs at beginning
+  else if (startIndex === 0) {
+    r || console.log('start');
+    str = '<span style="text-decoration: underline;">' + typed + '</span>' +
+          '<span style="color: #6E6E6E;">' + match.substring(typed.length) + '</span>';
+  }
+  // occurs in middle
+  else if (startIndex > 0) {
+    r || console.log('middle');
+    str = "<span style='color: #6E6E6E;'>" + match.substring(0, startIndex) + "</span>" +
+          "<span style='text-decoration: underline;'>" + match.substring(startIndex, startIndex + typed.length) + "</span>" +
+          "<span style='color: #6E6E6E;'>" + match.substring(startIndex + typed.length) + "</span>";
+  }
+  // occurs at the end
+  else {
+    r || console.log('end');
+    str = "<span class='completed'>" + typed.substring(0, startIndex) + "</span>" +
+          "<span class='typed'>" + match + "</span>";
+  }
+  return str;
+}
+
+})();
