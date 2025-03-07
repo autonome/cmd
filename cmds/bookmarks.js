0ad3781a28ec3548fcacd3db34985b2855b40dbd
dietrich ayala <me@burrito.space>
2025-03-07 13:34:09 +0100

    files

---
+++ cmds/bookmarks.js
@@ -0,0 +1,17 @@
+// browser (Firefox) vs chrome (Chromium)
+const api = typeof chrome != 'undefined' ? chrome : browser;
+
+const strings = {
+  name: 'bookmark current page',
+};
+
+export {
+  name: strings.name,
+  async execute() {
+    const tab = await browser.tabs.query({active:true});
+    await browser.bookmarks.create({
+      title: tab[0].title,
+      url: tab[0].url
+    });
+  }
+};
---
+++ cs/scanner.js
@@ -0,0 +1,18 @@
+const microformats = Microformats.get();
+console.log('mf', microformats);
+
+/*
+-function getSelectionText() {
+-  let text = "";
+-  if (window.getSelection) {
+-    text = window.getSelection().toString();
+-  } else if (document.selection && document.selection.type != "Control") {
+-    text = document.selection.createRange().text;
+-  }
+-  return text;
+-}
+-
+-
+-var selectedText = getSelectionText()
+-selectedText;
+*/
